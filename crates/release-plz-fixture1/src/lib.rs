b681d899354b781e2bbee431cae8add45bd66a23
Lorenzo Delgado <lnsdev@proton.me>
2024-10-12 00:14:13 +0200

    feat(lnsd-release-plz-fixture1): add a new function to API (nl) (#12)
    Signed-off-by: Lorenzo Delgado <lnsdev@proton.me>

--- crates/release-plz-fixture1/src/lib.rs
+++ crates/release-plz-fixture1/src/lib.rs
@@ -1,3 +1,7 @@
+#[deprecated(
+    since = "0.1.0",
+    note = "It will be removed in the next major release."
+)]
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -7,13 +11,36 @@ pub fn greet_en(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+/// Greets the given name in Dutch.
+pub fn greet_nl(name: &str) -> String {
+    format!("Hallo, {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_greet_en() {
+        //* Given
+        let name = "Alice";
+
+        //* When
+        let result = greet_en(name);
+
+        //* Then
+        assert_eq!(result, "Hello, Alice!");
+    }
+
+    #[test]
+    fn test_greet_nl() {
+        //* Given
+        let name = "Alice";
+
+        //* When
+        let result = greet_nl(name);
+
+        //* Then
+        assert_eq!(result, "Hallo, Alice!");
     }
 }
