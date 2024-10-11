4b91a3622c8c970d511422d62431d8f2271f6107
Lorenzo Delgado <lnsdev@proton.me>
2024-10-11 14:47:30 +0200

    feat(release-plz-fixture2): add a new function to API
    Signed-off-by: Lorenzo Delgado <lnsdev@proton.me>

--- crates/release-plz-fixture2/src/lib.rs
+++ crates/release-plz-fixture2/src/lib.rs
@@ -2,6 +2,11 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+/// Greets the given name in Spanish.
+pub fn greet_es(name: &str) -> String {
+    format!("¡Hola, {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
